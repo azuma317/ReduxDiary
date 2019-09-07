b2adbb8e6e5e14bc0f9c995fc98960af93e9b670
Azuma Sato <lifeghtcloerce5291@gmail.com>
2019-09-07 11:42:28 +0900

    Initial Commit

---
+++ ReduxDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReduxDiary
+//
+//  Created by さとうあずま on 2019/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
